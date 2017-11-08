6b475aa652dec08d88e1b7d7d02db25522f5c018
Alex Graves <graves@thedp.com>
2017-11-07 22:38:41 -0500

    finish up neighborhoods script

--- data/neighborhoods.py
+++ data/neighborhoods.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import pandas as pd
 from shapely.geometry import shape, Point
 # run pip install shapely
 
@@ -6,13 +8,34 @@ from shapely.geometry import shape, Point
 with open('neighborhoods.geojson') as f:
     js = json.load(f)
 
-# construct point based on lat/lng returned by geocoder
-# eventually this should take in all points in the given file
-# the output neighborhood will create a relation
-point = Point(-75.1953933,39.9483068)
+# load given data file
+if len(sys.argv) is not 2:
+    raise Exception('Please enter one CSV file name.')
 
-# check each polygon to see if it contains the point
-for feature in js['features']:
-    polygon = shape(feature['geometry'])
-    if polygon.contains(point):
-        print 'Point in', feature['properties']['name']
+input_file = sys.argv[1]
+output_file = 'neighborhoods_' + input_file
+data = pd.read_csv(input_file, encoding='utf8')
+output = pd.DataFrame(columns=['crime_id', 'neighborhood'])
+
+# check for lat/lng columns
+if 'lat' not in data.columns:
+	raise ValueError('Input has no latitude column')
+if 'lng' not in data.columns:
+	raise ValueError('Input has no longitude column')
+
+for index, row in data.iterrows():
+    # construct point based on lat/lng
+    lat = row['lat']
+    lng = row['lng']
+    point = Point(lng, lat)
+    output.set_value(index, 'crime_id', index)
+    output.set_value(index, 'neighborhood', None)
+
+    # find which neighborhood this point is in and save
+    for feature in js['features']:
+        polygon = shape(feature['geometry'])
+        if polygon.contains(point):
+            name = feature['properties']['name']
+            output.set_value(index, 'neighborhood', name)
+
+output.to_csv(output_file, index=False, encoding='utf8')
