a031a40b00669b8204015b1af0e06168f28aab56
andrea baric <abaric896@gmail.com>
2017-12-02 13:12:21 -0500

    connected to db

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mysql = require('mysql');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +23,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+var connection = mysql.createConnection({
+  host     : "rds-philly-guide.cww85eefiukb.us-east-1.rds.amazonaws.com",
+  user     : "phillyguideadmin",
+  password : "cis450f17",
+  port     : "3306"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+
+connection.end();
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
