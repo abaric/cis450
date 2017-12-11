84375fbc75c142981334038ae97e585a6c1a3f87
Andrea Baric <baric@seas.upenn.edu>
2017-12-11 15:27:10 -0500

    education query done

--- app.js
+++ app.js
@@ -23,77 +23,25 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/all', index);
+app.use('/education', index);
 app.use('/users', users);
 
 var connection = mysql.createConnection({
-  host     : "rds-philly-guide.cww85eefiukb.us-east-1.rds.amazonaws.com",
-  user     : "guest",
-  password : "cis450guest",
-  port     : "3306",
-  database:  "phillyguide"
+	host     : "rds-philly-guide.cww85eefiukb.us-east-1.rds.amazonaws.com",
+	user     : "guest",
+	password : "cis450guest",
+	port     : "3306",
+	database:  "phillyguide"
 });
-
-//connection.connect();
-
-app.get('/ll', function(req, res) {
-	console.log("before");
-    res.send('table: ' + req.query['all']);
-
-});
-
-// app.get('/a', function(req, res){
-
-// 	var table = req.query['all'];
-
-// 	function fetchID(data, callback) {
-//         connection.query('SELECT * from ' + table, function(err, rows) {
-//             if (err) {
-//                 callback(err, null);
-//             } else 
-//                 callback(null, rows);
-//         });
-// 	}
-
-// });
-
-// app.get('/al', function(req, res){
-
-// 	var table = req.query['all'];
-	
-// 	console.log(table);
-	
-// 	connection.query('SELECT * from ' + table, function(err, rows, fields) {
-// 	    if(err) console.log(err);
-	    
-// 	    console.log('The solution is: ', rows);
-// 	    connection.end();
-// 	});	
-// });
-
-// app.get('/',function(req,res){
-//     var table = req.query['table'];
-//     console.log(table);
-//     connection.query('SELECT * from ' + table, function(err, rows, fields) {
-//         if(err) {
-//           console.log(err);
-//           res.send(500);
-//         }    
-//         console.log('The solution is: ', JSON.parse(JSON.stringify(rows)));
-        
-//         res.send(rows);
-//     }); 
-// });
-
-
-
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,13 @@ var router = express.Router();
 
 var mysql = require('mysql');
 
-
 var connection = mysql.createConnection({
-  host     : "rds-philly-guide.cww85eefiukb.us-east-1.rds.amazonaws.com",
-  user     : "guest",
-  password : "cis450guest",
-  port     : "3306",
-  database:  "phillyguide"
+	host     : "rds-philly-guide.cww85eefiukb.us-east-1.rds.amazonaws.com",
+	user     : "guest",
+	password : "cis450guest",
+	port     : "3306",
+	database:  "phillyguide"
 });
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -19,44 +17,66 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/hi', function(req, res, next) {
+router.get('/all', function(req, res, next) {
 	var table = req.query['table'];
-    console.log(table);
-    connection.query('SELECT * from ' + table, function(err, rows, fields) {
-        if(err) {
-          console.log(err);
-          res.send(500);
-        }    
-        console.log('The solution is: ', JSON.parse(JSON.stringify(rows)));
-        res.json(JSON.parse(JSON.stringify(rows)));
-        //res.send(rows);
-    });  
-//  console.log(req.parser);
+	console.log(table);
+	connection.query('SELECT * from ' + table, function(err, rows, fields) {
+		if(err) {
+			console.log(err);
+			res.send(500);
+		}    
+		console.log('The solution is: ', JSON.parse(JSON.stringify(rows)));
+		res.json(JSON.parse(JSON.stringify(rows)));
+	});  
 });
+	
 
-
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-// 	res.render('index', { title: 'Philly Guide'});
+/* GET walk score. */
+// router.get('/walkscore', function(req, res) {
+// 	scoreOutput.find({}, function(err, result){
+// 		if (err) {
+// 			console.log(err);
+// 			res.send(500);
+// 		}
+// 		console.log('The solution is :');
+// 	});
+// 	res.send(result);
 // });
 
-/* GET home page. 
-router.get('/hi', function(req, res, next) {
+//"SELECT n.name, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category DESC LIMIT " + num_rows
 
-	var table = req.query['table'];
-    console.log(table);
-    connection.query('SELECT * from ' + table, function(err, rows, fields) {
-        if(err) {
-          console.log(err);
-          res.send(500);
-        }    
-        console.log('The solution is: ', JSON.parse(JSON.stringify(rows)));
-        res.render('index', { title: 'Philly Guide', table: rows });
-        //res.send(rows);
-    });  
-//  console.log(req.parser);
+
+router.get('/education', function(req, res, next) {
+	var field = req.query['option'];
+	var num_rows = req.query['education_num'];
+
+	connection.query("SELECT n.name, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category DESC LIMIT " + num_rows, function(err, rows, fields) {
+		if(err) {
+			console.log(err);
+			res.send(500);
+		}    
+		console.log('soln: ', JSON.parse(JSON.stringify(rows)));
+		res.json(JSON.parse(JSON.stringify(rows)));
+	});  
 });
-*/
+
+/* GET home page. */
+
+// router.get('/education', function(req, res, next) {
+// 	var metric = req.query['table1'];
+// 	console.log(metric);
+// 	var num = req.query['education_num'];
+
+// 	connection.query('SELECT ' + metric + 'from schools' + ' LIMIT ' + num, function(err, rows, fields) {
+// 		if(err) {
+// 			console.log(err);
+// 			res.send(500);
+// 		}    
+// 		console.log('The solution is: ', JSON.parse(JSON.stringify(rows)));
+// 		res.json(JSON.parse(JSON.stringify(rows)));
+// 	});  
+// });
+
+
 
 module.exports = router;
