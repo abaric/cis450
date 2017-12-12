4da597de287f948b3ee5249e7b675e7ad5b5deab
Andrea Baric <baric@seas.upenn.edu>
2017-12-11 22:54:20 -0500

    added crimes options

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 router.get('/all', function(req, res, next) {
 	var table = req.query['table'];
 	console.log(table);
-	connection.query('SELECT * from ' + table + ' LIMIT 10', function(err, rows, fields) {
+	connection.query('SELECT * from ' + table, function(err, rows, fields) {
 		if(err) {
 			console.log(err);
 			res.send(500);
@@ -43,13 +43,31 @@ router.get('/all', function(req, res, next) {
 // 	res.send(result);
 // });
 
-//"SELECT n.name, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category DESC LIMIT " + num_rows
+//'SELECT n.name, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id AND c.type = ' + "'" + field + "'" + ' GROUP BY n.id ORDER BY crimes' + order + 'LIMIT ' + rows;
 
+
+router.get('/crime', function(req, res, next) {
+	var field = req.query['crime_type'];
+	var rows = req.query['num_rows'];
+	var order = req.query['ordering'];
+
+	connection.query('SELECT n.name, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id AND c.type = ' + "'" + field + "'" + ' GROUP BY n.id ORDER BY crimes ' + order + ' LIMIT ' + rows, function(err, rows, fields) {
+		if(err) {
+			console.log(err);
+			res.send(500);
+		}    
+		console.log('soln: ', JSON.parse(JSON.stringify(rows)));
+		res.json(JSON.parse(JSON.stringify(rows)));
+	});  
+});
 
 router.get('/education', function(req, res, next) {
-	var field = req.query['option'];
+	var field = req.query['educ_type'];
 	var num_rows = req.query['education_num'];
 	var m = req.query['metric'];
+	console.log(field + "FIELD");
+	console.log(num_rows + "numROWS");
+	console.log(m + "metric");
 
 	connection.query("SELECT n.name, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category " + m + " LIMIT " + num_rows, function(err, rows, fields) {
 		if(err) {
@@ -60,22 +78,6 @@ router.get('/education', function(req, res, next) {
 		res.json(JSON.parse(JSON.stringify(rows)));
 	});  
 });
-
-// router.get('/education', function(req, res, next) {
-// 	var field = req.query['option'];
-// 	var num_rows = req.query['education_num'];
-// 	var metric = req_query['metric'];
-// 	console.log(metric + "METRIC");
-
-// 	connection.query("SELECT n.name, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category ASC LIMIT " + num_rows, function(err, rows, fields) {
-// 		if(err) {
-// 			console.log(err);
-// 			res.send(500);
-// 		}    
-// 		console.log('soln: ', JSON.parse(JSON.stringify(rows)));
-// 		res.json(JSON.parse(JSON.stringify(rows)));
-// 	});  
-// });
 
 
 
