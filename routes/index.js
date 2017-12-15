617d756501f7c69ace489ece1a134a8d24e0e0f4
Alex Graves <graves@thedp.com>
2017-12-15 13:07:01 -0500

    update popups to include type of data

--- routes/index.js
+++ routes/index.js
@@ -78,7 +78,7 @@ router.get('/walkscore', function(req, res, next) {
 /* GET walk score. */
 router.get('/walkscore-neighborhood', function(req, res, next) {
 	var id = req.query['id'];
-	
+
  	scoreOutput.aggregate([{$match: {'walkscore':{"$gte": +from_val, "$lte": +to_val}}},{$match: {'neighborhood_id':{"$ne": null}}},{$group: {_id: "$neighborhood_id", walkscore: {$avg: "$walkscore"}}}, {$sort:{'walkscore':-1}}, {$project:{walkscore: round('$walkscore',2)}}, {$limit:+neighborhoods}], function(err, result){
  		if (err) {
  			console.log(err);
@@ -91,7 +91,7 @@ router.get('/walkscore-neighborhood', function(req, res, next) {
 
 /* GET walk score. */
 router.get('/walkscore-all', function(req, res, next) {
-  
+
 	scoreOutput.aggregate([{$match: {'walkscore':{"$gte": + "0", "$lte": + "100"}}},{$match: {'neighborhood_id':{"$ne": null}}},{$group: {_id: "$neighborhood_id", walkscore: {$avg: "$walkscore"}}}, {$sort:{'walkscore':-1}}, {$project:{walkscore: round('$walkscore',2)}}, {$limit: + "200"}], function(err, result){
  		if (err) {
  			console.log(err);
@@ -107,7 +107,7 @@ router.get('/neighborhood', function(req, res, next) {
 	var id = req.query['id'];
 
 	connection.query('SELECT n.name FROM neighborhoods n WHERE n.id = ' + "'" + id + "'", function(err, rows, fields) {
-		if(err) {
+		if (err) {
 			console.log(err);
 			res.send(500);
 		}
@@ -122,22 +122,7 @@ router.get('/crime', function(req, res, next) {
 	var rows = req.query['num_rows'];
 	var order = req.query['ordering'];
 
-	connection.query('SELECT n.name, n.id, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id AND c.type = ' + "'" + field + "'" + ' GROUP BY n.id ORDER BY crimes ' + order + ' LIMIT ' + rows, function(err, rows, fields) {
-		if(err) {
-			console.log(err);
-			res.send(500);
-		}
-		console.log('soln: ', JSON.parse(JSON.stringify(rows)));
-		res.json(JSON.parse(JSON.stringify(rows)));
-	});
-});
-
-/* GET crime data for all-data */
-router.get('/crimes-all', function(req, res, next) {
-	var num_rows = req.query['rows'];
-	var order = req.query['ordering'];
-	
-	connection.query('SELECT n.name, n.id, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id GROUP BY n.id ORDER BY crimes ' + order + ' LIMIT ' + num_rows, function(err, rows, fields) {
+	connection.query('SELECT n.name, n.id, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id AND c.type LIKE ' + "'" + field + "'" + ' GROUP BY n.id ORDER BY crimes ' + order + ' LIMIT ' + rows, function(err, rows, fields) {
 		if (err) {
 			console.log(err);
 			res.send(500);
@@ -148,10 +133,8 @@ router.get('/crimes-all', function(req, res, next) {
 });
 
 /* GET crime data for all-data */
-router.get('/crimes-full', function(req, res, next) {
-	var field = req.query['field'];
-
-	connection.query('SELECT n.' + field + ', COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id GROUP BY n.id ORDER BY crimes', function(err, rows, fields) {
+router.get('/crimes-all', function(req, res, next) {
+	connection.query('SELECT n.name, n.id, COUNT(h.crime_id) as crimes FROM neighborhoods n, happened_in h, crimes c WHERE n.id = h.neighborhood_id AND c.id = h.crime_id GROUP BY n.id ORDER BY crimes', function(err, rows, fields) {
 		if (err) {
 			console.log(err);
 			res.send(500);
@@ -171,7 +154,7 @@ router.get('/education', function(req, res, next) {
 	console.log(m + "metric");
 
 	connection.query("SELECT n.name, n.id, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category " + m + " LIMIT " + num_rows, function(err, rows, fields) {
-		if(err) {
+		if (err) {
 			console.log(err);
 			res.send(500);
 		}
@@ -185,7 +168,7 @@ router.get('/education-all', function(req, res, next) {
 	var field = "graduation_rate";
 
 	connection.query("SELECT n.name, n.id, AVG(s." + field + ") as category FROM neighborhoods n, located_in l, schools s WHERE n.id = l.neighborhood_id AND s.id = l.school_id GROUP by n.id ORDER BY category DESC", function(err, rows, fields) {
-		if(err) {
+		if (err) {
 			console.log(err);
 			res.send(500);
 		}
